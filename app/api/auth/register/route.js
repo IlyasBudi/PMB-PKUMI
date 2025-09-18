4b88863253bb478bcb8d14c7b41decbe039d4248
IlyasBudi <wjati145@gmail.com>
2025-09-18 20:49:00 +0700

    fix:register mail verification

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -99,7 +99,9 @@ export async function POST(request) {
 
     // Send email verification
     let transporter = nodemailer.createTransport({
-      service: process.env.MAIL_SERVICE,
+      host: process.env.MAIL_HOST,
+      port: Number(process.env.MAIL_PORT),
+      secure: false, // true untuk port 465, false untuk 587
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
